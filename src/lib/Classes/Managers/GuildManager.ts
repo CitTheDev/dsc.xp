60972c0735b27fecf3ad8bed9f833e2e4f3072ce
Cit <shavinsept@gmail.com>
2022-11-22 17:31:52 +1100

    Fixed messy array handling

--- src/lib/Classes/Managers/GuildManager.ts
+++ src/lib/Classes/Managers/GuildManager.ts
@@ -22,10 +22,13 @@ export class GuildManager {
             const validate = validateGuildLeaderboardOptions(options);
             if (validate.invalid) return rej(new TypeError(validate.error));
 
-            // TODO: Fetch data first then use all methods
-            const sorted = (await DB.find({ guildId: options.guildId })).sort((a, b) => b.xp - a.xp).slice(0, options.limit - 1);
-            const data = sorted.map((user) => ({ guildId: user.guildId, userId: user.userId, level: user.level, xp: user.xp }));
-            return res(data);
+            const unsortedUserArray = await DB.find({ guildId: options.guildId });
+            const sorted = unsortedUserArray
+                .sort((a, b) => b.xp - a.xp)
+                .slice(0, options.limit - 1)
+                .map((user) => ({ guildId: user.guildId, userId: user.userId, level: user.level, xp: user.xp }));
+
+            return res(sorted);
         });
     }
 

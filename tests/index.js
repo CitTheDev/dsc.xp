b6280069e68119dd18cbce94e6b8c5d6b81b8ed9
Cit <shavinsept@gmail.com>
2022-11-25 16:12:09 +1100

    Add comments to test file

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,6 @@
 const { DiscordXP } = require("../dist/index.js");
-const { DATABASE_URL } = require("./config.json");
+const { DatabaseURL } = require("./config.json");
 
-const client = new DiscordXP(DATABASE_URL);
-console.log(client);
+const client = new DiscordXP(DatabaseURL);
+// TODO: Test before publishing to npm
+// Make dev tag on npm package
